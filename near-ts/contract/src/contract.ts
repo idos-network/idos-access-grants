90a1bacaa3c9ab440b0b5924b6e2ff68b52c393e
Paulo Köch <paulo.koch@gmail.com>
2023-09-11 17:27:48 +0100

    Keep behavior coherent between implementations

--- near-ts/contract/src/contract.ts
+++ near-ts/contract/src/contract.ts
@@ -141,10 +141,13 @@ export class AccessGrants {
       this.grantIdsByOwner.get(owner),
       this.grantIdsByGrantee.get(grantee),
       this.grantIdsByDataId.get(dataId),
-    ];
+    ].filter(Array.isArray);
+
+    if (grantIdSearches.length == 0) {
+      throw new Error("You must provide some search criteria")
+    }
 
     const grants = grantIdSearches
-      .filter(Array.isArray)
       .reduce((acc, val) => (acc.filter((id) => (val.includes(id)))))
       .map((id) => (this.grantsById.get(id)));
 
